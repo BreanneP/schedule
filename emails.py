31cdc6d76b3b4a466f56f0da76e2c1f1f0a8717a
BreanneP <bjpundsack@gmail.com>
2022-06-11 22:14:50 -0500

    added file functionality

--- emails.py
+++ emails.py
@@ -1,5 +1,6 @@
 import base64
 import mimetypes
+import os
 from apiclient import errors, discovery
 from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
@@ -16,6 +17,47 @@ def create_message(sender, to, subject, msg_html, msg_plain):
     msg.attach(MIMEText(msg_plain, 'plain'))
     msg.attach(MIMEText(msg_html, 'html'))
     raw = base64.urlsafe_b64encode(msg.as_bytes()).decode()
+    return {'raw': raw}
+
+
+def create_message_with_file(sender, to, subject, msg_html, msg_plain, attachment_file):
+    message = MIMEMultipart('mixed')
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    messageA = MIMEMultipart('alternative')
+    messageR = MIMEMultipart('related')
+
+    messageR.attach(MIMEText(msg_html, 'html'))
+    messageA.attach(MIMEText(msg_plain, 'plain'))
+    messageA.attach(messageR)
+    message.attach(messageA)
+
+    content_type, encoding = mimetypes.guess_type(attachment_file)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(attachment_file, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(attachment_file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(attachment_file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+
+    filename = os.path.basename(attachment_file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
     return {'raw': raw}
 
 
